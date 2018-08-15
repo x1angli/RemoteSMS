539862accc1e977ba61f95a73ecd33b5eaf98a19
Xiang Li <x@x1ang.li>
2018-08-15 12:17:57 +0800

    minor refactoring

--- app/src/main/java/li/x1ang/remotesms/receiver/SMSReceiver.kt
+++ app/src/main/java/li/x1ang/remotesms/receiver/SMSReceiver.kt
@@ -13,8 +13,6 @@ import li.x1ang.remotesms.PhoneMessage
 
 import li.x1ang.remotesms.service.SMSService
 import li.x1ang.remotesms.utils.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 /**
  * 短信接收处理
@@ -36,43 +34,31 @@ class SMSReceiver : BroadcastReceiver() {
     }
 
     private fun onSmsReceived(messages: List<SmsMessage>) {
-        val size = messages.size
-        Log.d("SMSReceiver","onReceive list $size")
-        val messageBody = StringBuilder()
-        val message: SmsMessage = messages[0]
+        Log.d("SMSReceiver","onReceive list $messages.size")
+        val smsHelper = SmsHelper()
+        val msgBody = smsHelper.getMsgBody(messages)
+        Log.d("SMSReceiver", "onReceive message $msgBody")
 
-        if (size > 1) {
-            //长短信拼接处理
-            messages.map {
-                messageBody.append(it.messageBody)
-            }
-        } else {
-            messageBody.append(message.messageBody)
-        }
-
-        Log.d("SMSReceiver", "onReceive message $messageBody")
-
+        val message = messages[0]
         message.let {
-            val receiver_id = getLocalPhoneNumber()
             val sender_id = it.displayOriginatingAddress ?: ""
 //            val contact = getContactName(context?.contentResolver, it.displayOriginatingAddress) ?: ""
             val timestamp = it.timestampMillis
-            val phoneMessage = PhoneMessage(timestamp, sender_id, messageBody.toString())
+            val phoneMessage = PhoneMessage(timestamp, sender_id, msgBody)
 
             val phoneFilter = App.inputPhone
             val contentFilter = App.inputContent
 
             val isNoFilter = TextUtils.isEmpty(phoneFilter) && TextUtils.isEmpty(contentFilter)
             val isSenderIDFilter = !TextUtils.isEmpty(phoneFilter) && sender_id.contains(phoneFilter, true)
-            val isContentFilter = !TextUtils.isEmpty(contentFilter) && messageBody.contains(contentFilter, true)
+            val isContentFilter = !TextUtils.isEmpty(contentFilter) && msgBody.contains(contentFilter, true)
 
             Log.d("SMSReceiver","转发规则 $isNoFilter | $isSenderIDFilter | $isContentFilter")
 
             if (isNoFilter || isSenderIDFilter || isContentFilter) {
-                val dateFormat = SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss", Locale.CHINA)
-                val msgMD = genMsgMarkdown(receiver_id, dateFormat.format(phoneMessage.timestamp), phoneMessage.sender_id, phoneMessage.content)
-                sendMsg(msgMD)
-                log("转发内容:\n $msgMD)}")
+                val msgMarkDown = smsHelper.genMsgMarkdown(phoneMessage)
+                smsHelper.sendMsg(msgMarkDown)
+                log("转发内容:\n $msgMarkDown)}")
             } else {
                 log("不符合转发规则 $phoneFilter | $contentFilter")
             }
--- app/src/main/java/li/x1ang/remotesms/utils/SMS.kt
+++ app/src/main/java/li/x1ang/remotesms/utils/SMS.kt
@@ -11,6 +11,7 @@ import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
 import android.provider.ContactsContract.PhoneLookup
+import android.telephony.SmsMessage
 import android.telephony.TelephonyManager
 
 import cz.msebera.android.httpclient.HttpStatus
@@ -22,10 +23,82 @@ import cz.msebera.android.httpclient.util.EntityUtils
 
 import li.x1ang.remotesms.App
 import li.x1ang.remotesms.DINGTALK_ENDPOINT
+import li.x1ang.remotesms.PhoneMessage
 
 /**
  * 短信工具类
  */
+
+
+/**
+ * 通过webhook上传短信
+ */
+
+
+
+class SmsHelper () {
+    private val DATE_FORMAT = SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss", Locale.CHINA)
+
+    var receiver_id = "123*3570"
+
+    init {
+    //获取本机手机号码,由于系统限制，并非所有手机都能获取到
+        receiver_id = getLocalPhoneNumber()
+    }
+
+    fun sendMsg(msg: String) {
+        thread {
+            val httpclient = HttpClients.createDefault()
+            val httpPost = HttpPost(DINGTALK_ENDPOINT)
+            httpPost.addHeader("Content-Type", "application/json; charset=utf-8")
+
+            val se = StringEntity(msg, "utf-8")
+            httpPost.entity = se
+
+            val response = httpclient.execute(httpPost)
+            if (response.statusLine.statusCode == HttpStatus.SC_OK) {
+                val result = EntityUtils.toString(response.entity, "utf-8")
+                log("sendMsg = $result")
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission", "HardwareIds")
+    fun getLocalPhoneNumber(): String {
+        val tm = App.context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        return tm.line1Number
+    }
+
+    fun genMsgMarkdown(phoneMessage: PhoneMessage): String {
+        var toReturn = ""
+        with(phoneMessage) {
+            toReturn = """{
+                "msgtype" : "markdown",
+                "markdown" :
+                    {
+                        "title" : "转发自: $receiver_id",
+                        "text"  : "#### 时间: ${DATE_FORMAT.format(timestamp)}\n
+                                   #### 来自: $sender_id\n
+                                   #### 正文: \n
+                                   > $content "
+                    }
+            }""".trimIndent()
+        }
+        return toReturn
+    }
+
+    fun getMsgBody(messages: List<SmsMessage>): String {
+        val messageBody = StringBuilder()
+
+        messages.map {
+            messageBody.append(it.messageBody)
+        }
+        return messageBody.toString()
+    }
+
+
+}
+
 
 /*
 val nameCache = HashMap<String, String>()
@@ -50,49 +123,3 @@ fun getContactName(contentResolver: ContentResolver?, number: String,
     return null
 }
 */
-
-
-
-fun genMsgMarkdown(receiver_id: String, sender_id: String,  time: String, content: String): String {
-
-    return """{
-        "msgtype" : "markdown",
-        "markdown" :
-            {
-                "title" : "转发自: $receiver_id",
-                "text"  : "#### 时间: $time\n
-                           #### 来自: $sender_id  \n
-                           #### 正文: \n
-                           > $content "
-            }
-    }""".trimIndent()
-}
-
-
-
-//获取本机手机号码,由于系统限制，并非所有手机都能获取到
-@SuppressLint("MissingPermission", "HardwareIds")
-fun getLocalPhoneNumber(): String {
-    val tm = App.context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-    return tm.line1Number
-}
-
-/**
- * webhook上传短信
- */
-fun sendMsg(msg: String) {
-    thread {
-        val httpclient = HttpClients.createDefault()
-        val httpPost = HttpPost(DINGTALK_ENDPOINT)
-        httpPost.addHeader("Content-Type", "application/json; charset=utf-8")
-
-        val se = StringEntity(msg, "utf-8")
-        httpPost.entity = se
-
-        val response = httpclient.execute(httpPost)
-        if (response.statusLine.statusCode == HttpStatus.SC_OK) {
-            val result = EntityUtils.toString(response.entity, "utf-8")
-            log("sendMsg = $result")
-        }
-    }
-}
